70a03d80c42aa355dac137899c65cc99dbc196af
kimdobie <kimdobie@gmail.com>
2022-11-10 08:35:20 -0600

    Remove coreUI

--- src/pages/ReadOnly.tsx
+++ src/pages/ReadOnly.tsx
@@ -1,12 +1,12 @@
 import React, { ReactElement } from 'react';
-import FeatureFlagsContextUI from '../components/FeatureFlagsUI';
+import { FeatureFlagsUI } from '../components';
 
 //type ReadOnlyPageProps = {};
 
 const ReadOnlyPage = (): ReactElement => (
   <div className='container'>
     <h1>Local Storage Flags - Read Only</h1>
-    <FeatureFlagsContextUI readonly />
+    <FeatureFlagsUI readonly />
   </div>
 );
 
